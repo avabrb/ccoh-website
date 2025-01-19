d584fc0b8a0c1d7296bfb0376f0f131d4dbb3862
nominr <nr58@rice.edu>
2025-01-18 23:33:07 -0600

    very basic homebar

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HomeBar } from './Components'; 
+
+export default function Home() {
+  return (
+    <div>
+      <HomeBar />
+      <h1>this is the home page.</h1>
+    </div>
+  );
+}
