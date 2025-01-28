4436a113e89c4abcbbdf91ed94e51ca9eac6c3b9
avabrb <apb7@rice.edu>
2025-01-28 15:37:22 -0600

    changes

--- src/national-days/NationalDaysController.jsx
+++ src/national-days/NationalDaysController.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DaysModel from './NationalDaysTest'
+import DaysModel from './NationalDaysVM'
 import "./NationalDays.css"
 
 export default function NationalDays() {
--- src/national-days/NationalDaysTest.jsx
+++ src/national-days/NationalDaysTest.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./NationalDays.css"
-/* Line that causes problems: 
-import data from "./example.json" */
+/* Line that causes problems: */
+import data from "./example.JSON" 
 
 const DaysModel = () => {
     return (
       <div className="page">
-        <h1>Explore the national and official celebration days of all countries with Consular offices in Houston.</h1>
-        {renderEvents}
+        <h1>Explore the nationasl and official celebration days of all countries with Consular offices in Houston.</h1>
+        <h1>{data}</h1>
       </div>
     );
   };
