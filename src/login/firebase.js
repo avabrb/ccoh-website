e8dd1571eca6f6ca247a792d42c4b6ec87cb6c27
nominr <nr58@rice.edu>
2025-02-22 18:32:35 -0600

    no secret

--- src/login/firebase.js
+++ src/login/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCRYNzy7fGtAJ35YB08TJaa11GH1Jq6-mY",
-  authDomain: "ccoh-website-1d1f9.firebaseapp.com",
-  projectId: "ccoh-website-1d1f9",
-  storageBucket: "ccoh-website-1d1f9.firebasestorage.app",
-  messagingSenderId: "56949729453",
-  appId: "1:56949729453:web:85b5cb14e665a4873dbe24",
-  measurementId: "G-46KQ778WVL"
+  apiKey: VITE_FB_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MSG_ID,
+  appId: VITE_APP_ID,
+  measurementId: VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
